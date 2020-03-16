17559705de98fceac34b927218fa968460e1f4ca
stucork@gmail.com <stucork@gmail.com>
2020-03-16 01:58:53 +0000

    Saved version

--- client_code/Tabulator/__init__.py
+++ client_code/Tabulator/__init__.py
@@ -34,112 +34,214 @@ class Tabulator(TabulatorTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    js.call_js('create_table', self, self._height, self._layout, self._pagination_size_selector, self._index)
-    js.call_js('explore', lambda x: x)
+    print(dir(self))
+    js.call_js('create_table',
+                self,
+                self._auto_columns,
+                self._header_align,
+                self._header_visible,
+                self._index,
+                self._layout,
+                self._moveable_columns,
+                self._pagination,
+                self._pagination_button_count,
+                self._pagination_size,
+                self._pagination_size_selector,
+                self._resizable_columns,
+                self._row_selectable
+              ) 
     # Any code you write here will run when the form opens.
     
-  @property
-  def index(self):
-    return self._index
-  
-  @index.setter
-  def index(self, value):
-    self._index = value
-    
-  @property
-  def writeback(self):
-    return self._writeback
-  
-  @index.setter
-  def writeback(self, value):
-    self._writeback = value
-    
-  @property
-  def index(self):
-    return self._index
-  
-  @index.setter
-  def index(self, value):
-    self._index = value
-    
-    
-  @property
-  def pagination_size_selector(self):
-    return self._pagination_size_selector
-  
-  @pagination_size_selector.setter
-  def pagination_size_selector(self, value):
-    if value == 'None':
-       self._pagination_size_selector = None
-    else:
-      value = value.replace(',', ' ')
-      self._pagination_size_selector = [int(i) for i in value.split()]
-  
-  @property
-  def layout(self):
-    return self._layout
-  
-  @layout.setter
-  def layout(self, value):
-    layouts = ('fitData', 'fitDataFill', 'fitDataStretch','fitColumns')
-    if value not in layouts:
-      raise ValueError(f"layout must be one of: {', '.join(layouts)}")
-    self._layout = value
+    def add_row(self, row={}, top=True, pos=None):
+        self._data.append(row)
+        js.call_js('add_row', self, row, top, pos)
 
-  @property
-  def columns(self):
-    return self._columns
+    def select_row(self, row):
+        pass
 
-  @columns.setter
-  def columns(self, value):
-    print('setting columns')
-    self._columns = value
-    js.call_js('set_columns', self, self._columns)
-    
-  @property
-  def data(self):
-    return self._data
+    def update_row(self, row):
+        pass
 
-  @data.setter
-  def data(self, value):
-    self._data = value
-    js.call_js('set_data', self, self._data)
-  
-  @property
-  def height(self):
-    return self._height
-  
-  @height.setter
-  def height(self, value):
-    self._height = value
-    if self.parent:
-      js.call_js('set_height', self, value)
 
-      
-  def row_selected(self, row):
-    try:
-      row = next(r for r in self._data if r.get(self.index, float('nan')) == row.get(self.index))
-    except StopIteration as e:
-      row = None
-    self.raise_event('row_selected', row=row)     
-    
-    
-  def row_click(self, row):
-    try:
-      row = next(r for r in self._data if r.get(self.index, float('nan')) == row.get(self.index))
-    except StopIteration as e:
-      row = None
-    self.raise_event('row_click', row=row)    
+    def set_group_by(self, field):
+        js.call_js('set_group_by', self, field)
 
-  
-  def add_row(self, row={}, top=True, pos=None):
-    self._data.append(row)
-    js.call_js('add_row', self, row, top, pos)
+        
+    @property
+    def index(self):
+        return self._index
 
-  def form_show(self, **event_args):
-    """This method is called when the HTML panel is shown on the screen"""
-    js.call_js('redraw', self)
+    @index.setter
+    def index(self, value):
+        self._index = value
 
-  def set_group_by(self, field):
-    js.call_js('set_group_by', self, field)
+    @property
+    def writeback(self):
+        return self._writeback
+
+    @index.setter
+    def writeback(self, value):
+        self._writeback = bool(value)
+
+    @property
+    def auto_columns(self):
+        return self._auto_columns
+
+    @auto_columns.setter
+    def auto_columns(self, value):
+        self._auto_columns = bool(value)
+
+    @property
+    def resizable_columns(self):
+        return self._resizable_columns
+
+    @resizable_columns.setter
+    def resizable_columns(self, value):
+        self._resizable_columns = bool(value)
+
+    @property
+    def moveable_columns(self):
+        return self._moveable_columns
+
+    @moveable_columns.setter
+    def moveable_columns(self, value):
+        self._moveable_columns = bool(value)
+
+    @property
+    def header_align(self):
+        return self._header_align
+
+    @header_align.setter
+    def header_align(self, value):
+        if value not in ('top', 'middle', 'bottom'):
+            value = 'middle'
+        self._header_align = value
+
+    @property
+    def header_visible(self):
+        return self._header_visible
+
+    @header_visible.setter
+    def header_visible(self, value):
+        self._header_visible = bool(value)
+
+    @property
+    def pagination(self):
+        return self._pagination
+
+    @pagination.setter
+    def pagination(self, value):
+        self._pagination = bool(value)
+
+    @property
+    def pagination_size(self):
+        return self._pagination_size
+
+    @pagination_size.setter
+    def pagination_size(self, value):
+        self._pagination_size = int(value)
+
+    @property
+    def pagination_button_count(self):
+        return self._pagination_button_count
+
+    @pagination_button_count.setter
+    def pagination_button_count(self, value):
+        self._pagination_button_count = int(value)
+
+    @property
+    def row_selectable(self):
+        return self._row_selectable
+
+    @row_selectable.setter
+    def row_selectable(self, value):
+        self._row_selectable = bool(value)
+
+    @property
+    def pagination_size_selector(self):
+        return self._pagination_size_selector
+
+    @pagination_size_selector.setter
+    def pagination_size_selector(self, value):
+        value = value.strip()
+        if value == 'None' or not value:
+            self._pagination_size_selector = None
+        else:
+            value = value.replace(',', ' ')
+            self._pagination_size_selector = [int(i) for i in value.split()]
+
+    @property
+    def layout(self):
+        return self._layout
+
+    @layout.setter
+    def layout(self, value):
+        layouts = ('fitData', 'fitDataFill', 'fitDataStretch', 'fitColumns')
+        if value not in layouts:
+            value = 'fitColumns'
+        self._layout = value
+
+    @property
+    def columns(self):
+        return self._columns
+
+    @columns.setter
+    def columns(self, value):
+        print('setting columns')
+        self._columns = value
+        js.call_js('set_columns', self, self._columns)
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        self._data = value
+        js.call_js('set_data', self, self._data)
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        self._height = value
+        if self.parent:
+            js.call_js('set_height', self, value)
+
+    def row_selected(self, row):
+        try:
+            row = next(r for r in self._data if r.get(
+                self.index, float('nan')) == row.get(self.index))
+        except StopIteration as e:
+            row = None
+        self.raise_event('row_selected', row=row)
+
+    def row_click(self, row):
+        try:
+            row = next(r for r in self._data if r.get(
+                self.index, float('nan')) == row.get(self.index))
+        except StopIteration as e:
+            row = None
+        self.raise_event('row_click', row=row)
+
+    def form_show(self, **event_args):
+        """This method is called when the HTML panel is shown on the screen"""
+        js.call_js('redraw', self)
+        raise_event('show')
+
+
+    def row_edited(self, js_row):
+        if writeback:
+            try:
+                row = next(row for row in self._data if row.get(
+                self.index, float('nan')) == js_row.get(self.index))
+                row.update(js_row)
+            except StopIteration as e:
+                row = None
+        else:
+            row = js_row
+        raise_event('row_edited', row=row)
+
     
