896e032c93d2502c95d5d654916819b408ae76a3
mrcork <stucork@gmail.com>
2020-03-24 17:52:26 +0800

    testreplace data

--- client_code/Tabulator/__init__.py
+++ client_code/Tabulator/__init__.py
@@ -285,6 +285,7 @@ class Tabulator(TabulatorTemplate):
         sender.interval = 0
         sender.remove_from_parent()
         anvil.js.call_js('redraw', self)
+        print('replace_data')
         anvil.js.call_js('replace_data', self._data)
         
     def redraw(self, **event_args):
