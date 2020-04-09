ea30416ada24c75f50f90ad7d8ad8ac4b4dbd230
stuart@aketo.no <stuart@aketo.no>
2020-04-09 01:27:16 +0000

    Saved automatically

--- client_code/Tabulator/__init__.py
+++ client_code/Tabulator/__init__.py
@@ -155,6 +155,7 @@ class Tabulator(TabulatorTemplate):
     def data(self, value):
         anvil.js.call_js('set_data', self, value)
         self._data = anvil.js.call_js('get_data', self)
+        print(self._data)
 
     @property
     def height(self):
