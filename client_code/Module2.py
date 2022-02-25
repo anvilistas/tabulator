7c610d66651273218f34ea27201117cbebae048d
stucork@gmail.com <stucork@gmail.com>
2022-02-25 10:40:38 +0000

    Edited new_file.html, settings, Tabulator5 and Module2

--- client_code/Module2.py
+++ client_code/Module2.py
@@ -1,4 +1,5 @@
-from anvil.js import import_from
+import anvil
+from anvil.js import import_from, get_dom_node
 from anvil.js.window import Function as _Function, document, Promise
 
 from operator import attrgetter
@@ -9,7 +10,7 @@ def _import(*attrs):
     attrs = attrs or ["default"]
     return attrgetter(*attrs)(module)
 
-Tabulator, Module, Accessor = _import("Tabulator", "Module", "AccessorModule")
+Tabulator, Module, Accessor, Format = _import("Tabulator", "Module", "AccessorModule", "FormatModule")
 
 transformRow = Accessor.prototype.transformRow
 
@@ -24,35 +25,36 @@ Accessor.prototype.initalize = lambda: print("OH WICKED")
 print(Accessor.prototype.transformRow)
 
 
-LoadModule = _Function("Module", "AnvilTabulator", "initializer", """
-class AnvilModule extends Module {
+Register = _Function("Tabulator", "Module", "cls", "name", """
+class CustomModule extends Module {
     constructor(table) {
         super(table);
-        AnvilTabulator(this, table);
-    }
-    initialize() {
-        this._anvil.initalize();
+        cls(this, table);
     }
 }
-AnvilModule.moduleName = "anvil";
-return AnvilModule;
-""")
+CustomModule.moduleName = name;
+Tabulator.registerModule(CustomModule);
+""").bind(None, Tabulator, Module)
 
 
-class AnvilTabulator:
+class AnvilTabulatorModule:
     def __init__(self, mod, table):
         self.mod, self.table = mod, table
-        mod._anvil = {"initalize": self.initialize}
-        mod.registerTableOption("anvilFoo", True)
-        self.data = []
+        mod.initialize = self.initialize
 
     def initialize(self):
+        raise NotImplementedError
+        
+
+class DataModule(AnvilTabulatorModule):
+    def initialize(self):
         print("being initialized")
+        self.mod.registerTableOption("anvilFoo", True)
         self.mod.subscribe("data-loading", self.data_loading)
         self.mod.subscribe("data-params", self.data_params)
         self.mod.subscribe("data-load", self.data_load)
         self.mod.subscribe("row-data-retrieve", self.transform_row)
-    
+
     def data_loading(self, *args):
         print("data-loading", *args, sep="\n", end="\n\n")
         return True
@@ -71,22 +73,35 @@ class AnvilTabulator:
         return {**prev}
 
 
-AnvilModule = LoadModule(Module, AnvilTabulator, AnvilTabulator.initialize)
+class ComponentFormatter(AnvilTabulatorModule):
+    def initialize(self):
+        self.mod.subscribe("cell-format", self.cell_format)
+
+    def cell_format(self, cell, prev):
+        print("cell-format", cell, prev, sep="\n", end="\n\n")
+        if isinstance(prev, anvil.Component):
+            return get_dom_node(prev)
+        return prev
+    
+
 Tabulator.registerModule(Accessor)
-Tabulator.registerModule(AnvilModule)
+Tabulator.registerModule(Format)
+Register(DataModule, "data")
+Register(ComponentFormatter, "anvilComponent")
 
 Tabulator.extendModule("accessor", "accessors", {
     'roundDown': lambda value, *args: (print(value, "HEY"), value)[1][0]
 })
 
 el = document.createElement("div")
+document.body.appendChild(el)
 
 tabledata = [
- 	{'id':1, 'name':"Oli Bob", 'age':"12", 'col':"red", 'dob':""},
- 	{'id':2, 'name':"Mary May", 'age':"1", 'col':"blue", 'dob':"14/05/1982"},
- 	{'id':3, 'name':"Christine Lobowski", 'age':"42", 'col':"green", 'dob':"22/05/1982"},
- 	{'id':4, 'name':"Brendon Philips", 'age':"125", 'col':"orange", 'dob':"01/08/1980"},
- 	{'id':5, 'name':"Margret Marmajuke", 'age':"16", 'col':"yellow", 'dob':"31/01/1999"},
+ 	{'id':1, 'name':"Oli Bob", 'age':"12", 'col':"red", 'dob':"14/05/1982"},
+#  	{'id':2, 'name':"Mary May", 'age':"1", 'col':"blue", 'dob':"14/05/1982"},
+#  	{'id':3, 'name':"Christine Lobowski", 'age':"42", 'col':"green", 'dob':"22/05/1982"},
+#  	{'id':4, 'name':"Brendon Philips", 'age':"125", 'col':"orange", 'dob':"01/08/1980"},
+#  	{'id':5, 'name':"Margret Marmajuke", 'age':"16", 'col':"yellow", 'dob':"31/01/1999"},
 ]
 
 
--- client_code/Tabulator5/__init__.py
+++ client_code/Tabulator5/__init__.py
@@ -1,9 +1,69 @@
 from ._anvil_designer import Tabulator5Template
 from anvil import *
+from anvil.js.window import String, RegExp
+
+_RE_SNAKE = RegExp("[^a-zA-Z0-9]+(.)", "g")
+
+def _snake_to_camel(s):
+    return String.prototype.replace.call(s, RE_SNAKE, lambda m, c, *_: c.upper())
+
+
+class TabulatorQueuedCall:
+    def __init__(self, attr, queue):
+        self.attr = attr
+        self.queue = queue
+
+    def __call__(self, *args, **kws):
+        self.queue.append([attr, args, kws])
+
 
 class Tabulator5(Tabulator5Template):
     def __init__(self, **properties):
-        # Set Form properties and Data Bindings.
-        self.init_components(**properties)
+        self._options = {}
+        self._el = None
+        self._queued = []
+        self._t = None
+        self._init = False
+    
+    def _initialize(self):
+        self._t = _Tabulator(self._el, self._options)
+        for attr, args, kws in self._queued:
+            getattr(self._t, attr)(*args, **kws)
+        
+    def __getattr__(self, attr):
+        if self._t is None:
+            return TabulatorQueuedCall(attr, self._queued)
+        return getattr(self._t, attr)
+    
+    @staticmethod
+    def _clean_key(item):
+        return toCamelCase(item[0]), item[1]     
+    
+    def _show(self, **event_args):
+        if self._t is None:
+            self._initialize()
+        
+    def define(self, **options):
+        if self._init:
+            raise RuntimeError("define must be called before the Tabulator component is on the screen")
+        self._options |= map(lambda item: [_snake_to_camel(item[0]), item[1]], options.items())
 
-        # Any code you write here will run when the form opens.
+    @property
+    def data(self):
+        if self._t is None:
+            return self._options.get("data")
+        return [self._t.getData()
+    
+    @data.setter
+    def data(self, value):
+        if self._t is None:
+            self._options["data"] = value
+        return self._t
+    
+    @property
+    def columns(self):
+        pass
+    
+    @columns.setter
+    def columns(self, value):
+        pass
