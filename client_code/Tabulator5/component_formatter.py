39159ecb516dbbe9b6bf2ea6d9f869c5db6d6aec
stucork@gmail.com <stucork@gmail.com>
2022-02-26 10:50:33 +0000

    Edited Tabulator5.component_formatter

--- client_code/Tabulator5/component_formatter.py
+++ client_code/Tabulator5/component_formatter.py
@@ -6,11 +6,25 @@ from .js_tabulator import AbstractModule, register_module
 class ComponentFormatter(AbstractModule):
     def initialize(self):
         self.mod.subscribe("cell-format", self.cell_format)
-        self.mod.subscribe("", self.cell_init)
+        self.mod.subscribe("cell-format", self.cell_init)
 
     def cell_format(self, cell, prev):
         if isinstance(prev, Component):
             return get_dom_node(prev)
         return prev
 
+#     	initializeColumn(column){
+# 		column.modules.format = this.lookupFormatter(column, "");
 
+# 		if(typeof column.definition.formatterPrint !== "undefined"){
+# 			column.modules.format.print = this.lookupFormatter(column, "Print");
+# 		}
+
+# 		if(typeof column.definition.formatterClipboard !== "undefined"){
+# 			column.modules.format.clipboard = this.lookupFormatter(column, "Clipboard");
+# 		}
+
+# 		if(typeof column.definition.formatterHtmlOutput !== "undefined"){
+# 			column.modules.format.htmlOutput = this.lookupFormatter(column, "HtmlOutput");
+# 		}
+# 	}
