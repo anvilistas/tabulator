aeb03ddb121eb93d105531a1ec1db94498da9536
stucork@gmail.com <stucork@gmail.com>
2022-03-01 22:53:22 +0000

    Edited Tabulator5

--- client_code/Tabulator5/__init__.py
+++ client_code/Tabulator5/__init__.py
@@ -96,14 +96,14 @@ class Tabulator5(Tabulator5Template):
         def raiser(*args):
             kws = dict(zip(call_sig, args))
             return self.raise_event(event, **kws)
-        self._handlers[handler] = raiser
+        self._handlers[(event, handler)] = raiser
         self.on(_toCamel(event), raiser)
 
     set_event_handler = add_event_handler
 
     def remove_event_handler(self, event, handler):
         super().remove_event_handler(event, handler)
-        self.off(_toCamel(event), self._handlers.get(handler))
+        self.off(_toCamel(event), self._handlers.pop((event, handler), None))
 
     def define(self, **options):
         if self._t is not None:
@@ -141,4 +141,103 @@ class Tabulator5(Tabulator5Template):
     role = _HtmlTemplate.role
     
     #### for the autocomplete
-    
+    def add_row(self, row, top=True, pos=None):
+        """add a row - ensure the row has an index"""
+        index = row.get(self._index)
+        if index is None:
+            raise KeyError(f"you should provide an index '{self._index}' for this row")
+        if self.get_row(index):
+            raise KeyError(f"The index '{self._index}' should be unique")
+
+        self._table.addRow(row, top, pos)
+
+    def delete_row(self, index):
+        """delete a row - the index of the row must be provided"""
+        self._table.deleteRow(index)
+
+    def update_row(self, index, row):
+        """update a row - the index of the row must be provided"""
+        try:
+            self._table.updateRow(index, row)
+        except Exception as e:
+            if "NotFoundError" in repr(e):
+                pass
+            else:
+                raise e
+
+    def get_row(self, index):
+        """get the row - the index of the row must be provided"""
+        row = self._table.getRow(index)
+        return dict(row.getData()) if row else None
+
+    def select_row(self, index_or_indexes):
+        self._table.selectRow(index_or_indexes)
+
+    def deselect_row(self, index_or_indexes=None):
+        """deselect a row (single index), rows (list of indexes) or all rows (no argument)"""
+        if index_or_indexes is None:
+            self._table.deselectRow()  # call it with an empty argument
+        else:
+            self._table.deselectRow(index_or_indexes)
+
+    def get_selected(self):
+        """returns a list of selected rows"""
+        return [dict(row) for row in self._table.getSelectedData()]
+
+    def add_data(self, data, top=False, pos=None):
+        """add data - use the keyword arguments to determine where in the table it gets added"""
+        self._table.addData(data, top, pos)
+
+    def update_or_add_data(self, data):
+        """checks each row and updates data if the row exists, otherwise creates a new row"""
+        self._table.updateOrAddData(data)
+
+    @maintain_scroll_position
+    def replace_data(self, data):
+        # useful to keep the datatable in the same place
+        """replace all data in the table"""
+        self._table.replaceData(data)
+
+    def set_filter(self, field, type=None, value=None):
+        """for multiple filters pass a list of dicts with keys 'field', 'type', 'value'"""
+        if callable(field):
+            filter_func = field
+            field = lambda data, params: filter_func(dict(data), **params)
+        self._table.setFilter(field, type, value)
+
+    def add_filter(self, field, type=None, value=None):
+        if callable(field):
+            filter_func = field
+            field = lambda data, params: filter_func(dict(data), **params)
+        self._table.addFilter(field, type, value)
+
+    def remove_filter(self, field=None, type=None, value=None):
+        self._table.removeFilter(field, type, value)
+
+    def get_filters(self):
+        return self._table.getFilters()
+
+    def clear_filter(self, *args):
+        """include an arg of True to clear header filters as well"""
+        self._table.clearFilter(*args)
+
+    def set_sort(self, column, ascending=True):
+        """first argument can also be a list of sorters [{'column':'name', 'ascending':True}]"""
+        if isinstance(column, list):
+            sorters = column
+            for sorter in sorters:
+                sorter["dir"] = "asc" if sorter.pop("ascending") else "desc"
+            self._table.setSort(sorters)
+        elif isinstance(column, str):
+            self._table.setSort(column, "asc" if ascending else "desc")
+        else:
+            raise TypeError(
+                "expected first argument to be a list of sorters or the column field name"
+            )
+
+    def clear_sort(self):
+        self._table.clearSort()
+        # reset the table data
+        debugger
+        data = self._table.getData()
+        self._table.setData(data)
